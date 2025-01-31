6413e7a1ff49ba52dee333d24acadcc09a8ed1f5
Dmitry Ilves <ilvesdmitry@gmail.com>
2025-01-31 14:50:35 +0200

    added test for authorization

--- tests/Login.api.spec.ts
+++ tests/Login.api.spec.ts
@@ -5,10 +5,14 @@ import { StatusCodes } from 'http-status-codes'
 test('Successful authorized with valid username and password and return status code OK', async ({
   request,
 }) => {
+  const requestBody = {
+    username: 'qweqwe',
+    password: '123',
+  }
   const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-    data: { username: 'qweasd', password: '123' },
+    data: requestBody,
   })
+  console.log('response status:', response.status())
   console.log('response body:', await response.json())
-  console.log('response headers:', response.headers())
   expect(response.status()).toBe(StatusCodes.OK)
 })
