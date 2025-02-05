ffed8c7776b1fa6a7260c60276565f9aecdb4927
Dmitry Ilves <ilvesdmitry@gmail.com>
2025-02-05 12:36:27 +0200

    feat: TL-11 prettier

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,13 +2,12 @@ import { expect, test } from '@playwright/test'
 import { LoginDto } from './DTO/LoginDto'
 import { StatusCodes } from 'http-status-codes'
 
-test.describe("Login tests", async () => {
-  test('Successful authorization', async ({request}) => {
-    const response = await request.post(
-      'https://backend.tallinn-learning.ee/login/student', {
-        data: LoginDto.createLoginWithCorrectData()
-      });
+test.describe('Login tests', async () => {
+  test('Successful authorization', async ({ request }) => {
+    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDto.createLoginWithCorrectData(),
+    })
 
-    expect(response.status()).toBe(StatusCodes.OK);
+    expect(response.status()).toBe(StatusCodes.OK)
   })
 })
